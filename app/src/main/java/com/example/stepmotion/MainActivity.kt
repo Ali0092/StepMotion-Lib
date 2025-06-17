a437b47c17e261b2dbf687e9bbc0a96b0e6b4085
Muhammad Ali <90325088+Ali0092@users.noreply.github.com>
2025-06-18 01:29:11 +0500

    completed basic dynamic horizontal stepper

--- app/src/main/java/com/example/stepmotion/MainActivity.kt
+++ app/src/main/java/com/example/stepmotion/MainActivity.kt
@@ -15,20 +15,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.stepmotion.ui.theme.StepMotionTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,38 +33,76 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StepMotionTheme {
-                StepMotion(modifier = Modifier.background(Color.White))
+                HorizontalStepper()
             }
         }
     }
 }
 
 @Composable
-fun StepMotion(modifier: Modifier = Modifier) {
-    Row(modifier = modifier.fillMaxSize().padding(horizontal = 16.dp), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
-        StepperItem("1", title = "Step 1")
-//        Spacer(modifier = Modifier.height(1.dp).weight(1f).background(Color.DarkGray))
-        StepperItem("2", title = "Step 2")
-//        Spacer(modifier = Modifier.height(1.dp).weight(1f).background(Color.DarkGray))
-//        StepperItem("3", title = "Step 3",true)
+fun HorizontalStepper(modifier: Modifier = Modifier) {
+    Column(modifier
+        .fillMaxSize()
+        .padding(top = 60.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+        StepperTopBar(items = listOf(1,2,3,4,5))
+        StepperBottomBar(items = listOf("Step1","Step2","Step3","Step4","Step5"))
+    }
+}
+
+@Composable
+fun StepperTopBar(modifier: Modifier = Modifier, items: List<Int>) {
+    Row(modifier = modifier
+        .fillMaxWidth()
+        .padding(horizontal = 16.dp)) {
+        items.forEachIndexed { index,it->
+            if (index == items.lastIndex) {
+                TopStepperSingleItem(text = items[index].toString(), isEndNode = true)
+            } else TopStepperSingleItem(Modifier.weight(1f), text = "1")
+        }
+    }
+
+}
+
+@Composable
+fun TopStepperSingleItem(modifier: Modifier = Modifier, text: String = "", isEndNode: Boolean = false) {
+    Row(modifier = modifier,verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+        Card(modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            colors = CardDefaults.cardColors(containerColor = Color.DarkGray)) {
+            Box(Modifier.fillMaxSize()) {
+                Text(text = text,modifier = Modifier.align(Alignment.Center), color = Color.White, fontSize = 18.sp)
+            }
+        }
+        if (!isEndNode) {
+            Spacer(modifier = Modifier
+                .weight(1f)
+                .height(2.dp)
+                .background(Color.DarkGray))
+        }
     }
 }
 
 
-//this item can be laterly triedn with canvas for more customizations etc
 @Composable
-fun StepperItem(text: String = "1", title: String = "Step 1",isLastItem: Boolean = false) {
-    Column(verticalArrangement = Arrangement.Center) {
-        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
-            Card(modifier = Modifier.size(50.dp), shape = CircleShape, colors = CardDefaults.cardColors(containerColor = Color.DarkGray)) {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    Text(modifier = Modifier.align(Alignment.Center), text = text,color = Color.White)
-                }
-            }
-            if (!isLastItem) Spacer(modifier = Modifier.height(1.dp).width(100.dp).background(Color.DarkGray))
+fun StepperBottomBar(modifier: Modifier = Modifier, items: List<String>) {
+    Row(modifier = modifier
+        .fillMaxWidth()
+        .padding(horizontal = 16.dp, vertical = 8.dp)) {
+        items.forEachIndexed { index,it->
+            if (index == items.lastIndex) {
+                BottomStepperSingleItem(text = items[index].toString(), isEndNode = true)
+            } else  BottomStepperSingleItem(modifier.weight(1f), text = items[index].toString())
         }
-        Text( text = title,color = Color.Black)
     }
+}
 
 
+@Composable
+fun BottomStepperSingleItem(modifier: Modifier = Modifier, text: String = "", isEndNode: Boolean = false) {
+    Row(modifier = modifier,verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+        Text(text = text, color = Color.DarkGray, fontSize = 16.sp)
+        if (!isEndNode) {
+            Spacer(modifier = Modifier.weight(1f))
+        }
+    }
 }
